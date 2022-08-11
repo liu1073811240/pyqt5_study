34d596ff2db8a1e55d01705d19624b3f58658997
1073811240 <1073811240@qq.com>
2022-08-11 16:54:25 +0800

    update

--- 07-计算器/Caculator_Btn.py
+++ 07-计算器/Caculator_Btn.py
@@ -10,14 +10,15 @@ from PyQt5.Qt import *
 # 对计算器按钮样式 进行设置
 
 class CalculatorBtn(QPushButton):
+
+    key_pressed = pyqtSignal(str, str)
     def __init__(self, parent=None, *args, **kwargs):
         super(CalculatorBtn, self).__init__(parent, *args, **kwargs)
         # self.setAutoExclusive(True)  # 设置按钮样式 自动排斥。有可能被父控件覆盖
 
-
     def resizeEvent(self, *args, **kwargs):
         super(CalculatorBtn, self).resizeEvent(*args, **kwargs)
-        print(self.width() / 2)
+        # print(self.width() / 2)
         self.setStyleSheet("""
                     QPushButton[bg='gray'] {
                         color: white;
@@ -26,11 +27,11 @@ class CalculatorBtn(QPushButton):
                     QPushButton[bg='gray']:hover {
                         background-color: rgb(150, 150, 150);
                     }
-                    QPushButton[bg='orange'] {
+                    QPushButton[bg='orange'], QPushButton[bg='equal'] {
                         color: white;
                         background-color: rgb(207, 138, 0);
                     }
-                    QPushButton[bg='orange']:hover {
+                    QPushButton[bg='orange']:hover, QPushButton[bg='equal']:hover {
                         background-color: rgb(238, 159, 0);
                     }
                     QPushButton[bg='orange']:checked {
@@ -45,12 +46,15 @@ class CalculatorBtn(QPushButton):
                         background-color: rgb(230, 230, 230);
                     }
                     QPushButton[bg] {
+                        font--size: 20px;
                         border-radius: %dpx;
                     }
                     
                 
                 """ % (min(self.width(), self.height()) / 2))
 
-
+    def mousePressEvent(self, *args, **kwargs) -> None:
+        super().mousePressEvent(*args, **kwargs)
+        self.key_pressed.emit(self.text(), self.property("role"))
 
 
--- 07-计算器/Caculator_Pane.py
+++ 07-计算器/Caculator_Pane.py
@@ -9,12 +9,62 @@ from PyQt5.Qt import *
 from resource.caculator import Ui_Form
 
 
+class Calculator(QObject):
+    def __init__(self, parent):
+        super(Calculator, self).__init__(parent)
+        # self.setParent(parent)
+
+        # 数字键位 num 运算符 operator
+        self.key_models = []
+
+    def parse_key_model(self, key_model):
+        # print(key_model)
+        if key_model['role'] == 'clear':
+            print("清空所有内容")
+            return None
+
+        if key_model['role'] == 'caculate':
+            print("计算所有内容")
+            return None
+
+        # print(key_model)
+        # num operator
+        if len(self.key_models) == 0:
+            if key_model["role"] == "num":
+                self.key_models.append(key_model)  # 判定如果是第一次添加数字，那么就加入列表
+                print(self.key_models)
+            return None
+        # print(self.key_models)
+
+        if key_model["role"] == self.key_models[-1]["role"]:
+            if key_model["role"] == "num":
+                if self.key_models[-1]["title"] != "0":  # 如果前面一个和后面一个都为数字、且前面一个为字符串零的话，就做拼接
+                    print('--111--')
+                    self.key_models[-1]["title"] += key_model["title"]
+                else:  # 如果前面一个和后面一个都为数字、且前面一个不为字符串零的话，就做替换
+                    print('2222')
+                    self.key_models[-1]["title"] = key_model["title"]
+                print(self.key_models)
+
+            # print(self.key_models)
+            if key_model["role"] == "operator":
+                self.key_models[-1]["title"] = key_model["title"]  # 如果前面一个和后面一个都为操作运算符的话，后面就替换前面
+        else:
+            self.key_models.append(key_model)
+
+        print(self.key_models)
+
 # qto
 class CaculatorPane(QWidget, Ui_Form):
     def __init__(self, parent=None, *args, **kwargs):
         super(CaculatorPane, self).__init__(parent, *args, **kwargs)
         self.setAttribute(Qt.WA_StyledBackground, True)  # 设置背景属性风格，让背景图片能够正常显示出来。
         self.setupUi(self)
+        self.calculator = Calculator(self)
+
+    def get_key(self, title, role):
+        # print(title, role)  # self.text(), self.property("role")
+        self.calculator.parse_key_model({"title": title, "role": role})  # 解析计算机的 值 及其相关属性
 
 
 if __name__ == '__main__':
